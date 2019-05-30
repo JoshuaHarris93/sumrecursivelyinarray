7215c259601742f32e4936d2d623149c5bc75a7e
JoshuaHarris93 <josh@emptychairproductions.com>
2019-05-30 20:34:14 +0000

    half finished test couldn't figure it out

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function sumItems(array) {
-  sumItems();
+  if (array.length === 0) {
+    return 0;
+  } else {
+    return array[0] + sumItems(array.slice(1));
+  }
 }
-
 module.exports = sumItems;
